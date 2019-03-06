3f1e9e505e1a2d0b5fb7a2bd578eb93e119d7db0
Sören Dobberschütz <40642560+sdobber@users.noreply.github.com>
2019-03-06 20:14:20 +0100

    Add files via upload
    # This is a patched version of the latest MNIST.jl file (downloaded on Mar 6, 2019).
    # Please see the original contribution at https://github.com/johnmyleswhite/MNIST.jl

---
+++ MNIST.jl
@@ -0,0 +1,119 @@
+# This is a patched version of the latest MNIST.jl file (downloaded on Mar 6, 2019).
+# Please see the original contribution at https://github.com/johnmyleswhite/MNIST.jl
+
+
+module MNIST
+    using Compat
+
+    export trainfeatures, testfeatures,
+           trainlabel, testlabel,
+           traindata, testdata
+
+    const IMAGEOFFSET = 16
+    const LABELOFFSET = 8
+
+    const NROWS = 28
+    const NCOLS = 28
+
+    const TRAINIMAGES = joinpath(
+        dirname(@__FILE__), "..", "data", "train-images.idx3-ubyte"
+    )
+    const TRAINLABELS = joinpath(
+        dirname(@__FILE__), "..", "data", "train-labels.idx1-ubyte"
+    )
+    const TESTIMAGES = joinpath(
+        dirname(@__FILE__), "..", "data", "t10k-images.idx3-ubyte"
+    )
+    const TESTLABELS = joinpath(
+        dirname(@__FILE__), "..", "data", "t10k-labels.idx1-ubyte"
+    )
+
+    function imageheader(@compat(filename::AbstractString))
+        io = open(filename, "r")
+        magic_number = bswap(read!(io, Array{@compat(UInt32)}))
+        total_items = bswap(read!(io, Array{@compat(UInt32)}))
+        nrows = bswap(read!(io, Array{@compat(UInt32)}))
+        ncols = bswap(read!(io, Array{@compat(UInt32)}))
+        #magic_number = bswap(read(io, @compat(UInt32)))
+        #total_items = bswap(read(io, @compat(UInt32)))
+        #nrows = bswap(read(io, @compat(UInt32)))
+        #ncols = bswap(read(io, @compat(UInt32)))
+        close(io)
+        return (
+            magic_number,
+            @compat(Int(total_items)),
+            @compat(Int(nrows)),
+            @compat(Int(ncols))
+        )
+    end
+
+    function labelheader(@compat(filename::AbstractString))
+        io = open(filename, "r")
+        magic_number = bswap(read!(io, Array{@compat(UInt32)}))
+        total_items = bswap(read!(io, Array{@compat(UInt32)}))
+        #magic_number = bswap(read(io, @compat(UInt32)))
+        #total_items = bswap(read(io, @compat(UInt32)))
+        close(io)
+        return magic_number, @compat(Int(total_items))
+    end
+
+    function getimage(@compat(filename::AbstractString), index::Integer)
+        io = open(filename, "r")
+        seek(io, IMAGEOFFSET + NROWS * NCOLS * (index - 1))
+        image_t = read!(io, Array{@compat(UInt8)}(undef, MNIST.NROWS, MNIST.NCOLS))
+        #image_t = read(io, @compat(UInt8), (MNIST.NROWS, MNIST.NCOLS))
+        close(io)
+        return image_t'
+    end
+
+    function getlabel(@compat(filename::AbstractString), index::Integer)
+        io = open(filename, "r")
+        seek(io, LABELOFFSET + (index - 1))
+        label = read!(io, Array{@compat(UInt8)}(undef,1))
+        #label = read(io, @compat(UInt8))
+        close(io)
+        return label
+    end
+
+    function trainimage(index::Integer)
+        convert(Array{Float64}, getimage(TRAINIMAGES, index))
+    end
+
+    function testimage(index::Integer)
+        convert(Array{Float64}, getimage(TESTIMAGES, index))
+    end
+
+    function trainlabel(index::Integer)
+        convert(Float64, getlabel(TRAINLABELS, index)[1])
+    end
+
+    function testlabel(index::Integer)
+        convert(Float64, getlabel(TESTLABELS, index)[1])
+    end
+
+    trainfeatures(index::Integer) = vec(trainimage(index))
+
+    testfeatures(index::Integer) = vec(testimage(index))
+
+    function traindata()
+        _, nimages, nrows, ncols = imageheader(TRAINIMAGES)
+        features = Array(Float64, nrows * ncols, nimages)
+        labels = Array(Float64, nimages)
+        for index in 1:nimages
+            features[:, index] = trainfeatures(index)
+            labels[index] = trainlabel(index)
+        end
+        return features, labels
+    end
+
+    function testdata()
+        _, nimages, nrows, ncols = imageheader(TESTIMAGES)
+        features = Array(Float64, nrows * ncols, nimages)
+        labels = Array(Float64, nimages)
+        for index in 1:nimages
+            features[:, index] = testfeatures(index)
+            labels[index] = testlabel(index)
+        end
+        return features, labels
+    end
+end # module
